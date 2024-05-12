bcf2c6b2dd48934bdefd14e5eb3648f0f2394fda
NTH <dungnq81@gmail.com>
2024-05-12 22:28:09 +0700

    fix bug

--- src/wp-config.php
+++ src/wp-config.php
@@ -31,9 +31,8 @@ if ( ! defined( 'FS_METHOD' ) ) {
 /** PHP Memory */
 const WP_MEMORY_LIMIT     = '512M';
 const WP_MAX_MEMORY_LIMIT = '512M';
-
-const DISALLOW_FILE_EDIT = true;
-const DISALLOW_FILE_MODS = false;
+const DISALLOW_FILE_EDIT  = true;
+const DISALLOW_FILE_MODS  = false;
 
 /* SSL */
 const FORCE_SSL_LOGIN = true;
