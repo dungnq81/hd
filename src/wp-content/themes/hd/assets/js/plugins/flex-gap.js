a8c301419f40e81eeb1794e7acc3fca4e76f1f21
NTH <dungnq81@gmail.com>
2024-05-29 22:06:05 +0700

    add assets

---
+++ src/wp-content/themes/hd/assets/js/plugins/flex-gap.js
@@ -0,0 +1,1 @@
+!function(){var e=document.createElement("div");e.style.display="flex",e.style.flexDirection="column",e.style.rowGap="1px",e.appendChild(document.createElement("div")),e.appendChild(document.createElement("div")),document.body.appendChild(e);var t=1===e.scrollHeight;e.parentNode.removeChild(e),t&&document.documentElement.classList.add("flex-gap-support")}();
